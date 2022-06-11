e29c7ee245abfaab8f8658ffedf7eaca32f8a167
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-11 22:42:25 +0530

    Commiting in UC1

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -7,19 +7,43 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
-            Console.WriteLine("Enter value for x1");
+            Console.WriteLine("Enter value for x1 for first line");
             int x1 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value for x2");
+            Console.WriteLine("Enter value for x2 for first line");
             int x2 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value for y1");
+            Console.WriteLine("Enter value for y1 for first line");
             int y1 = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value for y2");
+            Console.WriteLine("Enter value for y2 for first line");
             int y2 = Int32.Parse(Console.ReadLine());
             int x = (x2 - x1) * (x2 - x1);
             int y = (y2 - y1) * (y2 - y1);
             int z = x + y;
-            double length = Math.Sqrt(z);
-            Console.WriteLine("Length of the line is" + length);
+            double length1 = Math.Sqrt(z);
+            Console.WriteLine("Length of the first line is" + length1);
+
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+            Console.WriteLine("Enter value for x1 for first line");
+            int x3 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value for x2 for first line");
+            int x4 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value for y1 for first line");
+            int y3 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value for y2 for first line");
+            int y4 = Int32.Parse(Console.ReadLine());
+            int a = (x2 - x1) * (x2 - x1);
+            int b = (y2 - y1) * (y2 - y1);
+            int c = x + y;
+            double length2 = Math.Sqrt(c);
+            Console.WriteLine("Length of the first line is" + length2);
+
+            if(length1 == length2)
+            {
+                Console.WriteLine("Both the lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both the lines are not equal");
+            }
 
         }
     }
